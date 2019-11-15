72275a769618f25a60b136691f26724298d40907
LABO <LABO@MacBook-Pro-LABO.local>
2019-11-15 16:00:56 +0100

    Add paper-plance icon to footer of MessageView

--- imports/ui/components/MessageView.tsx
+++ imports/ui/components/MessageView.tsx
@@ -5,13 +5,19 @@ import StyledMessageView from '../elements/StyledMessageView';
 import Header from './Header';
 import MessageBox from './MessageBox';
 import Avatar from './Avatar';
-import Searchbar from './Searchbar';
 import { Chat } from '../../api/models';
 
 const avatar_url:string = "https://randomuser.me/api/portraits/thumb/men/1.jpg";
 
 const MessageView = (props:any):JSX.Element => {
     const selectedChat:Chat = props.selectedChat;
+    const [inputValue, setInputValue] = React.useState<string>("");
+    const [iconName, setIconName] = React.useState<string>("microphone");
+    const handleChange = (e):void => {
+        setInputValue(e.target.value);
+        const name = e.target.value !== "" ? "paper-plane" : "microphone";
+        setIconName(name);
+    }
     return (
         <StyledMessageView>
             <Header iconClass="greyIcon" icons={["search", "paperclip", "ellipsis-v"]}>
@@ -22,7 +28,7 @@ const MessageView = (props:any):JSX.Element => {
                 </div>
             </Header>
             <MessageBox selectedChat={props.selectedChat} />
-            <Header iconsWidthSmall iconClass="iconFooter" icons={["microphone"]}>
+            <Header iconsWidthSmall iconClass="iconFooter" icons={[iconName]}>
                 <FontAwesome 
                     name="smile"
                     className="iconFooter"
@@ -32,6 +38,8 @@ const MessageView = (props:any):JSX.Element => {
                         className="message--input" 
                         type="text"
                         placeholder="Taper un message"
+                        value={inputValue}
+                        onChange={handleChange}
                     />
                 </label>
             </Header>
