8b96a1958ed7e73616a1bd4f2c8614e9c7492ee4
LABO <LABO@MacBook-Pro-LABO.local>
2019-11-18 23:17:15 +0100

    Done with Re ordering ChatList Items

--- imports/ui/components/ChatList.tsx
+++ imports/ui/components/ChatList.tsx
@@ -1,27 +1,42 @@
 import React from 'react';
+import FlipMove from 'react-flip-move';
 
 import StyledChatList from '../elements/StyledChatList';
 import ChatItem from './ChatItem';
 import Spinner from './Spinner';
+import {Chat} from '../../api/models';
 
-const ChatList = (props:any):JSX.Element => {
-    const { onChatClick, selectedChat, chats, chatsLoading } = props;
-    const renderChatItem = ():JSX.Element[] => {
-        return chats.map(chat => {
-            const active:boolean = selectedChat._id === chat._id;
+const ChatList = (props : any) : JSX.Element => {
+    const {onChatClick, selectedChat, chats, chatsLoading} = props;
+    const renderChatItem = () : JSX.Element[] => {
+        return chats.sort(function (a : Chat, b : Chat) {
+            // Turn your strings into dates, and then subtract them to get a value that is
+            // either negative, positive, or zero.
+            return b
+                .lastMessage
+                .createdAt
+                .getTime() - a
+                .lastMessage
+                .createdAt
+                .getTime();
+        }).map((chat : Chat) => {
+            const active : boolean = selectedChat._id === chat._id;
             return (
-                <ChatItem 
-                    key={chat._id}
-                    {...chat}
-                    onChatClick={onChatClick}
-                    active={active}
-                />
+                <React.Fragment key={chat._id}>
+                    <ChatItem 
+                        {...chat} 
+                        onChatClick={onChatClick} 
+                        active={active}
+                    />
+                </React.Fragment>
             )
         });
     }
     return (
         <StyledChatList>
-            {renderChatItem()}
+            <FlipMove>
+                {renderChatItem()}
+            </FlipMove>
         </StyledChatList>
     )
 }
